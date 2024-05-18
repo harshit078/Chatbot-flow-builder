ba21759b9f878a21cf0e733dfc07d3b2e8da4e16
harshit078 <harshitsingh467@gmail.com>
2024-05-18 19:04:28 +0530

    Initiated Settings and node pages

---
+++ src/components/Elements/Message.jsx
@@ -0,0 +1,24 @@
+import { BsChatText } from "react-icons/bs";
+import { TextContainer } from "./styled";
+
+import React from "react";
+
+export const NodesPanel = () => {
+  const onDragStart = (event, nodeType) => {
+    event.dataTransfer.setData("application/reactflow", nodeType);
+    event.dataTransfer.effectAllowed = "move";
+  };
+
+  return (
+    <button
+      className="dndnode"
+      onDragStart={(event) => onDragStart(event, "message")}
+      draggable
+    >
+      <TextContainer>
+        <BsChatText />
+        <span>Message</span>
+      </TextContainer>
+    </button>
+  );
+};
